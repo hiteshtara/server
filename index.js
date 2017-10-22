4abde7328fcc9b8f18ffc2c6719013bcb1d681b6
hitesh <mukadder@bu.edu>
2017-10-21 21:36:39 -0400

    used

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
+const cookieSession = require('cookie-session');
 require('./models/User');
 require('./services/passport');
+const passport = require('passport');
 mongoose.connect(keys.mongoURI);
 
 const authRoutes = require('./routes/authRoutes');
 // it wants to listen  to 5000 on your machine routed through node to express
 const app = express();
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 authRoutes(app);
 const PORT = process.env.PORT || 5000;
 // heroku will inject port ||5000;
