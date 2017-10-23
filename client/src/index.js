e0bb9ed11cc1666fe321972842d71dc1aeada19e
hitesh <mukadder@bu.edu>
2017-10-22 21:52:07 -0400

    added rducers to the reducx store

--- client/src/index.js
+++ client/src/index.js
@@ -5,8 +5,9 @@ import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import reducers from './reducers';
 // store takes an arrow function which is list of combined reducers initial state and middleware
-const store = createStore(() => [], {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware());
 // top level is provider as provider connects react to react redux
 ReactDOM.render(
   // make sure Provider component contains store and child of provider is app component
---
+++ client/src/reducers/index.js
@@ -0,0 +1,4 @@
+import { combineReducers } from 'redux';
+import authReducer from './authReducer';
+// whate ever keys we put in here are given to the state object
+export default combineReducers({});
