da2ef6a7269014c0a5fd055553cff2af2b10e9e1
hitesh <mukadder@bu.edu>
2017-10-22 22:18:00 -0400

    added landing componet to the router when user visit route root

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,24 @@
 import React from 'react';
-import {} from 'react-router-dom';
+// Browser router is brain
+//route determines which componet to show
+import { BrowserRouter, Route } from 'react-router-dom';
+
+// lets create dummy componets
+const Header = () => <h2>Header</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 const App = () => {
-  return <div>Hi there</div>;
+  // browser router can have maximum one div childern
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path="/" component={Landing} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 export default App;
