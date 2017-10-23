7fddef94a6e201103ef60a28a0c43fc9cb0c2f37
hitesh <mukadder@bu.edu>
2017-10-22 20:43:17 -0400

    wred redux store

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+// store takes an arrow function which is list of combined reducers initial state and middleware
+const store = createStore(() => [], {}, applyMiddleware());
+// top level is provider as provider connects react to react redux
+ReactDOM.render(
+  // make sure Provider component contains store and child of provider is app component
+  // provider makes sure when the store is changed all the child components will know
+  // and update child compnets with new state
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
