64373959c854960f6a22f46945be53f64df0e6dd
hitesh <mukadder@bu.edu>
2017-10-23 20:00:46 -0400

    deleted keys

--- config/keys.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  googleClientID:
-    '568229570768-or0m4i44e8t2osuf3b06pilidaf3eai7.apps.googleusercontent.com',
-  googleClientSecret: 'IBRhEIkYT1ABYLn-DHAbZ_4W',
-  mongoURI: 'mongodb://mukadder:dl2sg9779@ds227565.mlab.com:27565/emaily-dev',
-  cookieKey: 'loda'
-};
